1d7cfc3623d3deaa23edb3dd295d273e738d03e1
Vanessa <vanessa.aloe@internet-idee.net>
2021-05-31 17:24:41 +0200

    Update POM with new dependecies

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,10 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions( plugin = {"pretty",
+        "html:target/cucumber-reports/cucumber-report.html",
+        "json:target/cucumber-reports/cucumber.json"
+}, features = "src/test/resources/feature/Calculator.feature", stepNotifications = true, glue = "src/test/java")
+public class TestRunner {
+}
